d3f2cc2062e8921c28e6931399e5477876c27322
diancitie666 <1695941113@qq.com>
2025-04-05 14:43:31 +0800

    Add server.c and client.c

--- src/client.c
+++ src/client.c
@@ -19,25 +19,195 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include <getopt.h>
+#include <stdlib.h>
+
+
+void print_usage(char *progname);
+
+int server_client_init(const char *server_ip, int server_port);
 
 
 
-int server_client_init(char server_ip, int server_port);
-
-
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-	int			rv=0;
+
+	int             		server_port=0;
+	int						ch=0;
+	int             		connfd=-1;
+	char					*server_ip=NULL;
+	char					buf[200];
+
+	char					*w_message="ajbfnfabmknfbanba";
+
+	int						rv=-1;
+	struct sockaddr_in		serv_addr;
+
+	struct option   opts[]={
+		{"ip",   required_argument, NULL, 'i'},    // -i 或 --ip， 需参数（服务器地址）
+		{"port", required_argument, NULL, 'p'},     // -p 或 --port，需参数（服务器端口号）
+		{"help", no_argument, NULL, 'h'},           // -h 或 --help，无需参数
+		{NULL, 0, NULL, 0}                          // 结束标记
+	};
+
+	while((ch = getopt_long(argc, argv, "i:p:h", opts, NULL)) != -1)
+	{
+		switch(ch)
+		{
+			case 'i':
+					server_ip = strdup(optarg);  // 复制字符串
+					break;
+			case 'p':
+					server_port = atoi(optarg);      //若选项为 -p/--port，将参数值转换为整数并赋值给 port
+					break;
+			case 'h':
+					print_usage(argv[0]);   //若选项为 -h/--help，调用 print_usage 并返回 0。
+					return 0;
+			default:
+					print_usage(argv[0]);
+					return -1;
+		}
+	}
+
+	if (!server_ip) 
+	{
+		print_usage(argv[0]);
+		return -2;
+	}
+
+	if(!server_port)
+	{
+		print_usage(argv[0]);
+		return -3;
+	}
+					      
+				  
+	if( (connfd=server_client_init(server_ip, server_port)) < 0)
+	{
+		printf("connect to server [%s:%d] dailure: %s\n",*server_ip, server_port, strerror(errno));
+		return -4;
+	}
 	
-	int			listenfd;
+	while(1)
+	{
+		if( write(connfd, w_message, strlen(w_message)) < 0 )
+		{
+			printf("Write data to server [%s:%d] failure: %s\n", server_ip, server_port, strerror(errno));
+	    	close(connfd);
+			return -5;
+		}
 
+		memset(buf, 0, sizeof(buf));
+		rv = read(connfd, buf, sizeof(buf));
+		if(rv < 0)
+		{
+			printf("Read data from server [%s:%d] failure: %s\n", server_ip, server_port, strerror(errno));
+			close(connfd);
+			return -6;
+		}
 
+		else if( 0 == rv)
+		{
+			printf("Client connect to server get disconnected\n");
+			close(connfd);
+			return -7;
+		}	
+		printf("Read %d bytes data from server: %s\n", rv, buf);
+	}
+
+CleanUp:
+	close(connfd);
 	
+}
 
 
 
 
 
+
+
+int server_client_init(const char *server_ip, int server_port)
+{
+	struct sockaddr_in		serv_addr;
+	int						connfd;
+
+	connfd=socket(AF_INET, SOCK_STREAM, 0);
+	if(connfd < 0)
+	{
+		printf("Create socket failure: %s",strerror(errno));
+		return -1;
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(server_port);
+	
+	if( inet_aton(server_ip, &serv_addr.sin_addr) == 0) 
+	{
+		printf("Invalid IP address: %s\n", server_ip);
+		close(connfd);
+		return -2;						     
+	}
+	 
+	if( connect(connfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("Connect to server [%s:%d] failure: %s\n", server_ip, server_port, strerror(errno));
+		close(connfd);	
+		return -3;
+	}
+
+	return connfd;
 
 }
+
+
+int client_write(int connfd, const char *w_message, const char *server_ip, int server_port)
+{
+	if( write(connfd, w_message, strlen(w_message)) < 0 )    
+	{
+		printf("Write data to server [%s:%d] failure: %s\n", server_ip, server_port, strerror(errno)); 
+		close(connfd);
+		return -1;
+	} 
+
+	return 0;
+}
+
+int client_read(int connfd, char *buf, int buf_size, const char *server_ip, int server_port)
+{
+	int				rv=-1;
+
+	memset(buf, 0, buf_size);
+
+	rv=read(connfd, buf, buf_size-1);
+	if(rv < 0)
+	{
+		printf("Read data from server [%s:%d] failure: %s\n", server_ip, server_port, strerror(errno));
+		close(connfd);
+		return -1;
+	}
+
+	else if( 0 == rv)
+	{
+		printf("Client connect to server get disconnected\n");
+		close(connfd);
+		return -2;
+	}
+
+	buf[rv] = '\0';
+	return rv;
+}
+
+
+
+void print_usage(char *progname)
+{
+	printf("%s usage method:\n",progname);
+	printf("-i(--ip):Specify server IP address to connect\n");
+	printf("-p(--port): Specify server port to connect \n");
+	printf("-h(--help):Printf this help information.\n");
+	return ;
+}
+
+
 
--- src/server.c
+++ src/server.c
@@ -12,37 +12,175 @@
  ********************************************************************************/
 
 #include<stdio.h>
-//#include<server.h>
-
+#include <libgen.h>  
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
-#include "usage.h"
-
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#define	 Max_event	512
 
 int socket_server_init(char *listen_ip,int listen_port);
+void print_usage(char *progname);
+
 
 int main(int argc, char **argv)
 {
-	int 		 lis_port =8888;
-	int			 rv=-5;
-	int			 connfd=-1;
-	usage_message(argc, argv);
-	rv=socket_server_init(NULL,lis_port);
-	printf("rv=%d\n",rv);
+	char					*progname=NULL;
+	int						rv=-5;
+	int						connfd=-1;
+
+	int 					i=0;
+	char					buf[200];
 	
+	int            		    serv_port=0;
+	int           		    ch=0;
+	int						listenfd;
+
+	int						epollfd;			//epoll对象
+	struct epoll_event		event;				//events,data
+	struct epoll_event		event_array[Max_event];
+	int						events;
+
+	struct option   opts[]={
+		{"port", required_argument, NULL, 'p'},     // -p 或 --port，需参数（端口号）
+	    {"help", no_argument, NULL, 'h'},           // -h 或 --help，无需参数
+        {NULL, 0, NULL, 0}                          // 结束标记
+    };  
+	
+	progname = basename(argv[0]);
+
+	while((ch = getopt_long(argc, argv, "p:h", opts, NULL)) != -1) 
+	{
+		switch(ch)
+		{   
+			case 'p':
+					serv_port=atoi(optarg);      //若选项为 -p/--port，将参数值转换为整数并赋值给 port
+					break;
+			case 'h':
+					print_usage(argv[0]);   //若选项为 -h/--help，调用 print_usage 并返回 0。
+					return -1; 
+			default:
+					break;
+		}   
+	}   
+
+	if(!serv_port)
+	{   
+		print_usage(argv[0]);
+		return -1; 
+	}   
+
+	if( (listenfd=socket_server_init(NULL, serv_port)) < 0)   //创建服务器文件描述符socket
+	{
+		printf("Error: %s server listen on port %d failure\n", argv[0], serv_port);
+		return -2;
+	}
+
+	printf(" %s server start to listen on port %d \n", argv[0], serv_port);
+	
+	if( (epollfd=epoll_create(Max_event)) < 0) //创建epoll对象
+	{
+		printf("epoll_create() failure: %s\n", strerror(errno));
+		return -3;
+	}
+
+	event.events = EPOLLIN;
+	event.data.fd = listenfd;
+	if( epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &event) < 0) //向epoll对象中添加套接字listenfd
+	{
+		printf("epoll add listen socket failure: %s\n", strerror(errno));
+		return -4;
+	}
+
+	for( ; ; )
+	{
+		events = epoll_wait(epollfd, event_array, Max_event, -1);  //阻塞等待事件发生
+		if(events < 0)
+		{
+			printf("epoll failure: %s\n",strerror(errno));
+			break;
+		}
+
+		if(events == 0)
+		{
+			printf("epoll get timeout\n");
+			continue;
+		}
+
+		for(i=0; i<events; i++)
+		{
+			if( (event_array[i].events&EPOLLERR) || (event_array[i].events&EPOLLHUP) )
+			{
+				printf("epoll_wait get error on fd[%d]: %s\n", event_array[i].data.fd, strerror(errno));
+				epoll_ctl(epollfd, EPOLL_CTL_DEL, event_array[i].data.fd, NULL);
+				close(event_array[i].data.fd);
+			}
+
+			if(event_array[i].data.fd == listenfd)   //listenfd发生事件，即有新客户端连接
+			{
+				if( (connfd=accept(listenfd, (struct sockaddr *)NULL, NULL)) < 0)
+				{
+					printf("accept new client failure: %s\n", strerror(errno));
+					continue;
+				}
+
+				event.data.fd = connfd;
+				event.events = EPOLLIN|EPOLLOUT;
+				
+				if( epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &event) < 0)
+				{
+					printf("epoll add client socket failure: %s\n", strerror(errno));
+					close(event_array[i].data.fd);
+					continue;
+				}
+				printf("epoll add new client socket[%d] ok.\n", connfd);
+
+			}
+			
+			
+			else  //已连接的客户端有数据收发
+			{
+				if( (rv=read(event_array[i].data.fd, buf, sizeof(buf))) <= 0)
+				{
+					printf("socket[%d] read failure or get disconnect and will be removed.\n", event_array[i].data.fd);
+					epoll_ctl(epollfd, EPOLL_CTL_DEL, event_array[i].data.fd, NULL);
+					close(event_array[i].data.fd);
+					continue;
+				}
+
+				printf("socket[%d] read get %d bytes data\n", event_array[i].data.fd, rv);
+
+				if( write(event_array[i].data.fd, buf, rv) < 0)
+				{
+					printf("socket[%d] write failure: %s\n", event_array[i].data.fd, strerror(errno));
+					epoll_ctl(epollfd, EPOLL_CTL_DEL, event_array[i].data.fd, NULL);
+					close(event_array[i].data.fd);
+				}
+			}
+
+			
+		}
+	}
+CleanUp:
+	close(listenfd);
 	return 0;
 }
+				
+
+int socket_server_init(char *listen_ip,int listen_port);
+
 
 int socket_server_init(char *listen_ip,int listen_port)
 {
 	struct sockaddr_in			servaddr;
 	int 						rv=0;
-	int							on=-1;
+	int							on=1;
 	int							listenfd;
 
 	if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -68,7 +206,7 @@ int socket_server_init(char *listen_ip,int listen_port)
 		{
 			printf("inet_pton() set listen IP address failure:%s\n",strerror(errno));
 			rv = -2;
-			goto CleanUp;
+			close(listenfd);
 		}
 	}
 
@@ -76,23 +214,29 @@ int socket_server_init(char *listen_ip,int listen_port)
 	{
 		printf("Use bind() to  bind the TCP socket failure:%s\n",strerror(errno));
 		rv = -3;
-		goto CleanUp;
+		close(listenfd);
 	}
 
 	if(listen(listenfd, 200) < 0)
 	{
 		printf("Use listen() to listen listenfd failure:%s",strerror(errno));
 		rv = -4;
-		goto CleanUp;
+		close(listenfd);
 	}
 
-CleanUp:
-	if(rv < 0)
-		close(listenfd);
-	else
-		rv = listenfd;
-
+	rv = listenfd;
 	return rv;
 
 }
+
+
+void print_usage(char *progname)
+{
+	printf("%s usage method:\n",progname);
+	printf("-p(--port):sepcify serveer listen port.\n");		 
+	printf("-h(--help):printf this help information.\n");
+	return ;
+}
+
+
 
